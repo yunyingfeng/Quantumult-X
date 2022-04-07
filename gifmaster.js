f40dcc026b016486fa1117ada20ee71be907144d
云影风 <87920233+yunyingfeng@users.noreply.github.com>
2022-04-07 21:49:50 +0800

    Update gifmaster.js

--- gifmaster.js
+++ gifmaster.js
@@ -26,10 +26,10 @@ const vip = '/v1/user';
 
 if (urlq.indexOf(vip) != -1) {
 
-    obj.data.vip = 2;
-    obj.data.vip_start = 2022-03-03;
-    obj.data.uid = 622072f68e1753051c3d377a;
-    obj.data.vip_end = 2999-03-03;
+    obj.data.["vip"] = "2";
+    obj.data.["vip_start"] = "2022-03-03";
+    obj.data.["uid"] = "622072f68e1753051c3d377a";
+    obj.data.["vip_end"] = "2999-03-03";
 
     body = JSON.stringify(obj);
 
