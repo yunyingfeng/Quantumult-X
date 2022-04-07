62cea136b7f7f38083367c7ad0030c198735dedb
云影风 <87920233+yunyingfeng@users.noreply.github.com>
2022-04-07 21:42:20 +0800

    Update gifmaster.js

--- gifmaster.js
+++ gifmaster.js
@@ -3,7 +3,7 @@
 
 #gif制作
 
-^http://gifmaster.orzjun.com/v1/user/login url script-response-body https://raw.githubusercontent.com/yunyingfeng/Quantumult-X/main/gifmaster.js
+^http://gifmaster.orzjun.com/v1/user url script-response-body https://raw.githubusercontent.com/yunyingfeng/Quantumult-X/main/gifmaster.js
 
 
 [mitm] 
@@ -20,13 +20,13 @@ var obj = JSON.parse(body);
 
 
 
-const vip = '/v1/user/login';
+const vip = '/v1/user';
 
 
 
 if (urlq.indexOf(vip) != -1) {
 
-    obj.data.vip = 1;
+    obj.data.vip = 2;
     obj.data.vip_start = 2022-03-03;
     obj.data.uid = 622072f68e1753051c3d377a;
     obj.data.vip_end = 2999-03-03;
