d0aea634b4695b92623e5bc9e2cc2bebf5f25f62
云影风 <87920233+yunyingfeng@users.noreply.github.com>
2022-04-08 16:05:24 +0800

    Update xinyutingshu.js

--- xinyutingshu.js
+++ xinyutingshu.js
@@ -13,9 +13,9 @@ hostname = i.xinyulib.com.cn
 
 
 var objc = JSON.parse($response.body);
-objc.data.vipstartTime = "2020-09-28";
-objc.data.vipendtime = "2099-09-28";
-objc.data.trueName = "🌈云影风";
+objc.data.vipstartTime = "2020-12-12";
+objc.data.vipendtime = "2099-12-12";
+objc.data.trueName = "\ud83c\udf08\u4e91\u5f71\u98ce";
 objc.data.headImg = "https:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32\/Q3auHgzwzM7yAXdMr7eg6VhdfShXbwqcmib7NR8j2ztRYhs2qAWRjW1Fy13sEWrLv4ibCdRWVB23qxxjict8PBnYVLB5IqMDG4T/132";
 
 
