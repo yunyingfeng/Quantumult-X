1cab0b8a5fb243e91cc5e9d2b9416916feae1a22
云影风 <87920233+yunyingfeng@users.noreply.github.com>
2022-04-08 22:05:45 +0800

    Update quannengsmb.js

--- quannengsmb.js
+++ quannengsmb.js
@@ -4,20 +4,23 @@
 [rewrite_local]
 
 # > 全能扫描宝
-^https?:\/\/ddstar\.palmmob\.com\/palmmob3_40\/user\/GetUInfo url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/XinYuLibraryProCrack.js
+^https?:\/\/ddstar\.palmmob\.com\/palmmob3_40\/user\/GetUInfo url script-response-body https://raw.githubusercontent.com/yunyingfeng/Quantumult-X/main/quannengsmb.js
 
 [mitm] 
 
-hostname = i.xinyulib.com.cn
+hostname = ddstar.palmmob.com
 
 */
 
 
 var objc = JSON.parse($response.body);
-objc.data["vipstartTime"] = "2020-09-28";
-objc.data["vipendtime"] = "2999-09-28";
-objc.data["trueName"] = "\u6D65\u8F7B\u5C18\uD835\uDC36\uD835\uDC5F\uD835\uDC4E\uD835\uDC50\uD835\uDC58";
-objc.data["headImg"] = "https:\/\/thirdqq.qlogo.cn\/g?b=oidb&k=iaODNibZcQQTN0QPGShfkKIw&s=640";
+
+objc.data.vipexpiretime = "32502002651";
+objc.data.nickname = "\ud83c\udf08\u4e91\u5f71\u98ce";
+objc.data.headurl = "https:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32\/Q3auHgzwzM7yAXdMr7eg6VhdfShXbwqcmib7NR8j2ztRYhs2qAWRjW1Fy13sEWrLv4ibCdRWVB23qxxjict8PBnYVLB5IqMDG4T/132";
+
+
+
 $done({
     body : JSON.stringify(objc)
 });
