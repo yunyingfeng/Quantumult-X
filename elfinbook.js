abb83ad0d93138b8f5488a954f9f48be7a82a112
云影风 <87920233+yunyingfeng@users.noreply.github.com>
2022-04-08 14:26:37 +0800

    Update elfinbook.js

--- elfinbook.js
+++ elfinbook.js
@@ -15,14 +15,14 @@ hostname = api.elfinbook.com
 var body = $response.body;
 
 
-    obj.data.["vip"] = "1";
-    obj.data.["headimg_url"] : "https:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32/Q3auHgzwzM7yAXdMr7eg6VhdfShXbwqcmib7NR8j2ztRYhs2qAWRjW1Fy13sEWrLv4ibCdRWVB23qxxjict8PBnYVLB5IqMDG4T/132";
+    obj.data["vip"] = "1";
+    obj.data["headimg_url"] : "https:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32/Q3auHgzwzM7yAXdMr7eg6VhdfShXbwqcmib7NR8j2ztRYhs2qAWRjW1Fy13sEWrLv4ibCdRWVB23qxxjict8PBnYVLB5IqMDG4T/132";
     
     
         
     obj.subscription["expire_time"] : "2029.01.01";
     obj.subscription["nickname"] : "🌈云影风";
-    obj.subscription["invite_code"] : "Ue2GnXhkCN";
+    
   
     
     
