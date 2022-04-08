0069317b118c72173ece9a5dd1b5d6f5d740cd4e
云影风 <87920233+yunyingfeng@users.noreply.github.com>
2022-04-08 14:13:52 +0800

    Update elfinbook.js

--- elfinbook.js
+++ elfinbook.js
@@ -3,7 +3,7 @@
 
 #elfinbook易飞
 
-^https?:\/\/api\.elfinbook\.com url script-response-body https://raw.githubusercontent.com/yunyingfeng/Quantumult-X/main/elfinbook.js
+^https?:\/\/api\.elfinbook\.com\/v4\/user\/detail url script-response-body https://raw.githubusercontent.com/yunyingfeng/Quantumult-X/main/elfinbook.js
 
 [mitm] 
 hostname = api.elfinbook.com
@@ -15,14 +15,14 @@ hostname = api.elfinbook.com
 var body = $response.body;
 
 
-    obj.data.vip = 1;
-    obj.data.headimg_url : "https://thirdwx.qlogo.cn/mmopen/vi_32/Q3auHgzwzM7yAXdMr7eg6VhdfShXbwqcmib7NR8j2ztRYhs2qAWRjW1Fy13sEWrLv4ibCdRWVB23qxxjict8PBnYVLB5IqMDG4T/132";
+    obj.data.["vip"] = "1";
+    obj.data.["headimg_url"] : "https://thirdwx.qlogo.cn/mmopen/vi_32/Q3auHgzwzM7yAXdMr7eg6VhdfShXbwqcmib7NR8j2ztRYhs2qAWRjW1Fy13sEWrLv4ibCdRWVB23qxxjict8PBnYVLB5IqMDG4T/132";
     
     
         
-        obj.subscription["expire_time"] : "2999.01.01";
-        obj.subscription["nickname"] : "🌈云影风";
-        obj.subscription["invite_code"] : "Ue2GnXhkCN";
+    obj.subscription["expire_time"] : "2029.01.01";
+    obj.subscription["nickname"] : "🌈云影风";
+    obj.subscription["invite_code"] : "Ue2GnXhkCN";
   
     
     
