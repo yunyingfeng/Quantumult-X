f368133be7a443d856ef408617d12a4583909211
云影风 <87920233+yunyingfeng@users.noreply.github.com>
2022-04-08 22:19:36 +0800

    Update quannengsmb.js

--- quannengsmb.js
+++ quannengsmb.js
@@ -14,8 +14,8 @@ hostname = ddstar.palmmob.com
 
 
 var objc = JSON.parse($response.body);
-
-objc.data.vipexpiretime = "32502002651";
+objc.data.channel = 2;
+objc.data.vipexpiretime = "32480575451";
 objc.data.nickname = "\ud83c\udf08\u4e91\u5f71\u98ce";
 objc.data.headurl = "https:\/\/thirdwx.qlogo.cn\/mmopen\/vi_32\/Q3auHgzwzM7yAXdMr7eg6VhdfShXbwqcmib7NR8j2ztRYhs2qAWRjW1Fy13sEWrLv4ibCdRWVB23qxxjict8PBnYVLB5IqMDG4T/132";
 
