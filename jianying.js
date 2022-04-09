f3ed18ede337ab993b5270707412ce48947a7594
云影风 <87920233+yunyingfeng@users.noreply.github.com>
2022-04-09 17:00:59 +0800

    Update jianying.js

--- jianying.js
+++ jianying.js
@@ -11,14 +11,14 @@ hostname = buy.itunes.apple.com,
 */
 
 var body = $response.body;
-var urlq = $request.url;
+var url = $request.url;
 var obj = JSON.parse(body);
 
 const vip = '/verifyReceipt';
 
-if (urlq.indexOf(vip) != -1) {
-    obj.date.web_order_line_item_id = "888888800000000";
-    obj.date.original_transaction_id = "88888800000000";
+if (url.indexOf(vip) != -1) {
+    obj.web_order_line_item_id = "888888800000000";
+    obj.original_transaction_id = "88888800000000";
   
   
     body = JSON.stringify(obj);
