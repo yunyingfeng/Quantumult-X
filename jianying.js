fa81f139023c3d565b36f1800f24ea6346aa358a
云影风 <87920233+yunyingfeng@users.noreply.github.com>
2022-04-09 17:59:12 +0800

    Update jianying.js

--- jianying.js
+++ jianying.js
@@ -5,7 +5,7 @@
 
 [rewrite_local]
 
-^https\:\/\/buy.itunes.apple.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/yunyingfeng/Quantumult-X/main/jianying.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/yunyingfeng/Quantumult-X/main/jianying.js
 
 [mitm]
 hostname = buy.itunes.apple.com,
@@ -13,9 +13,8 @@ hostname = buy.itunes.apple.com,
 */
 
 let obj = JSON.parse($response.body);
-
 obj = 
-  {
+{
   "status": 0,
   "environment": "Production",
   "receipt": {
@@ -24,33 +23,33 @@ obj =
     "app_item_id": 1000000000,
     "bundle_id": "com.mediaeditor.video",
     "application_version": "437",
-    "download_id": 8888888888888,
+    "download_id": 99999999999999,
     "version_external_identifier": 999999999,
-    "receipt_creation_date": "2199-12-31 11:57:45 Etc/GMT",
-    "receipt_creation_date_ms": "7258046265000",
-    "receipt_creation_date_pst": "2199-12-31 11:57:45 America/Los_Angeles",
-    "request_date": "2020-10-10 10:10:10 Etc/GMT",
-    "request_date_ms": "1602295810000",
-    "request_date_pst": "2020-10-10 10:10:10 America/Los_Angeles",
-    "original_purchase_date": "2020-10-10 10:10:10 Etc/GMT",
-    "original_purchase_date_ms": "1602295810000",
-    "original_purchase_date_pst": "2020-10-10 10:10:10 America/Los_Angeles",
+    "receipt_creation_date": "2111-11-11 11:11:11 Etc/GMT",
+    "receipt_creation_date_ms": "4476655324000",
+    "receipt_creation_date_pst": "2111-11-11 11:11:11 America/Los_Angeles",
+    "request_date": "2011-11-11 11:11:11 Etc/GMT",
+    "request_date_ms": "1555555555555",
+    "request_date_pst": "2011-11-11 11:11:11 America/Los_Angeles",
+    "original_purchase_date": "2011-11-11 11:11:11 Etc/GMT",
+    "original_purchase_date_ms": "1555555555555",
+    "original_purchase_date_pst": "2011-11-11 11:11:11 America/Los_Angeles",
     "original_application_version": "433",
     "in_app": [{
       "quantity": "1",
       "product_id": "yearautorenew",
-      "transaction_id": "888888888888888",
-      "original_transaction_id": "888888888888888",
+      "transaction_id": "520000631442654",
+      "original_transaction_id": "520000631442654",
       "purchase_date": "2011-11-11 11:11:11 Etc/GMT",
-      "purchase_date_ms": "1602295810000",
-      "purchase_date_pst": "2020-10-10 10:10:10 America/Los_Angeles",
-      "original_purchase_date": "2020-10-10 10:10:10 Etc/GMT",
-      "original_purchase_date_ms": "1602295810000",
-      "original_purchase_date_pst": "2020-10-10 10:10:10 America/Los_Angeles",
-      "expires_date": "2199-12-31 11:57:45 Etc/GMT",
-      "expires_date_ms": "7258046265000",
-      "expires_date_pst": "2199-12-31 11:57:45 America/Los_Angeles",
-      "web_order_line_item_id": "888888888888888",
+      "purchase_date_ms": "1555555555555",
+      "purchase_date_pst": "2011-11-11 11:11:11 America/Los_Angeles",
+      "original_purchase_date": "2011-11-11 11:11:11 Etc/GMT",
+      "original_purchase_date_ms": "1555555555555",
+      "original_purchase_date_pst": "2011-11-11 11:11:11 America/Los_Angeles",
+      "expires_date": "2111-11-11 11:11:11 Etc/GMT",
+      "expires_date_ms": "4476655324000",
+      "expires_date_pst": "2111-11-11 11:11:11 America/Los_Angeles",
+      "web_order_line_item_id": "520000240955544",
       "is_trial_period": "true",
       "is_in_intro_offer_period": "false"
     }]
@@ -58,18 +57,18 @@ obj =
   "latest_receipt_info": [{
     "quantity": "1",
     "product_id": "yearautorenew",
-    "transaction_id": "888888888888888",
-    "original_transaction_id": "888888888888888",
-    "purchase_date": "2020-10-10 10:10:10 Etc/GMT",
-    "purchase_date_ms": "1602295810000",
-    "purchase_date_pst": "2020-10-10 10:10:10 America/Los_Angeles",
-    "original_purchase_date": "2020-10-10 10:10:10 Etc/GMT",
-    "original_purchase_date_ms": "1602295810000",
-    "original_purchase_date_pst": "2020-10-10 10:10:10 America/Los_Angeles",
-    "expires_date": "2199-12-31 11:57:45 Etc/GMT",
-    "expires_date_ms": "7258046265000",
-    "expires_date_pst": "2199-12-31 11:57:45 America/Los_Angeles",
-    "web_order_line_item_id": "888888800000000",
+    "transaction_id": "520000631442654",
+    "original_transaction_id": "520000631442654",
+    "purchase_date": "2011-11-11 11:11:11 Etc/GMT",
+    "purchase_date_ms": "1555555555555",
+    "purchase_date_pst": "2011-11-11 11:11:11 America/Los_Angeles",
+    "original_purchase_date": "2011-11-11 11:11:11 Etc/GMT",
+    "original_purchase_date_ms": "1555555555555",
+    "original_purchase_date_pst": "2011-11-11 11:11:11 America/Los_Angeles",
+    "expires_date": "2111-11-11 11:11:11 Etc/GMT",
+    "expires_date_ms": "4476655324000",
+    "expires_date_pst": "2111-11-11 11:11:11 America/Los_Angeles",
+    "web_order_line_item_id": "520000000000000",
     "is_trial_period": "true",
     "is_in_intro_offer_period": "false",
     "subscription_group_identifier": "99999999"
@@ -77,10 +76,9 @@ obj =
   "latest_receipt": "5L2c6ICFQGtpbmc=",
   "pending_renewal_info": [{
     "auto_renew_product_id": "yearautorenew",
-    "original_transaction_id": "888888800000000",
+    "original_transaction_id": "520000000000000",
     "product_id": "yearautorenew",
     "auto_renew_status": "1"
   }]
 }
-  
-  $done({body : JSON.stringify(objc)});
+$done({body: JSON.stringify(obj)});
