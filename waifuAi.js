d049cdcba9960e21ec4f5cfe0acc5f16187f5017
云影风 <87920233+yunyingfeng@users.noreply.github.com>
2024-02-11 09:37:07 +0800

    更新 waifuAi.js

--- waifuAi.js
+++ waifuAi.js
@@ -1,7 +1,7 @@
 /*************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https:
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/yunyingfeng/Quantumult-X/main/waifuAi.js
 
 [mitm]
 hostname = api.revenuecat.com
