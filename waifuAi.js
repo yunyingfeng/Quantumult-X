3541915e1cf014c3a35305b34554cbdd3b4ed774
云影风 <87920233+yunyingfeng@users.noreply.github.com>
2024-02-11 09:35:50 +0800

    更新 waifuAi.js

---
+++ waifuAi.js
@@ -0,0 +1,29 @@
+/*************************************
+
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https:
+
+[mitm]
+hostname = api.revenuecat.com
+
+*************************************/
+
+
+let obj={
+  "request_date": "2024-02-10T15:05:48Z",
+  "request_date_ms": 1707577548030,
+  "subscriber": {
+    "entitlements": {},
+    "first_seen": "2020-04-10T15:04:23Z",
+    "last_seen": "2024-02-10T15:04:23Z",
+    "management_url": null,
+    "non_subscriptions": {},
+    "original_app_user_id": "60933B06-48C5-4B36-B506-6A932C54ECC1",
+    "original_application_version": "68",
+    "original_purchase_date": "2020-04-10T16:00:22Z",
+    "other_purchases": {},
+    "subscriptions": {}
+  }
+}
+
+$done({ body: JSON.stringify(obj), status: 200 });
